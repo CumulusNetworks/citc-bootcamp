e0d2ec57bb8ff43cf69b804aceb8da737877e694
Eric Pulvino <ericpulvino@gmail.com>
2018-01-17 23:47:24 -0500

    updated for syntax error with pip.

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -qy  python-dev libssl-dev  sshpass build-essential libffi-dev
 sudo pip install setuptools --upgrade
-sudo pip install ansible=2.4.1.0
+sudo pip install ansible==2.4.1.0
 rm -rf ansible-push-keys
 git clone https://github.com/cumulusnetworks/ansible-push-keys
 cd ansible-push-keys; cat /etc/dhcp/dhcpd.hosts | grep 'host .* {' | cut -d " " -f 2 >> hosts
